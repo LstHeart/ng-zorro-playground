2a3312608a3a01c5be330159fdcf552476bd0d62
LstHeart <dennis.lstheart@qq.com>
2022-09-14 10:00:45 +0000

    update

--- table/src/app/table/table.component.ts
+++ table/src/app/table/table.component.ts
@@ -24,7 +24,7 @@ export class TableComponent {
     },
     {
       key: '3',
-      name: 'Joe Black',
+      name: 'Joe Black3',
       age: 32,
       tel: '0575-22098909',
       phone: 18900010002,
@@ -45,6 +45,54 @@ export class TableComponent {
       tel: '0575-22098909',
       phone: 18900010002,
       address: 'Dublin No. 2 Lake Park'
+    }
+  ];
+
+  listFormatData = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      tel: '0571-22098909',
+      phone: 18889898989,
+      address: 'New York No. 1 Lake Park',
+      rowSpan: null
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      tel: '0571-22098333',
+      phone: 18889898888,
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      rowSpan: null
+    },
+    {
+      key: '3',
+      name: 'Joe Black3',
+      age: 32,
+      tel: '0575-22098909',
+      phone: 18900010002,
+      address: 'Sidney No. 1 Lake Park',
+      rowSpan: 3,
+    },
+    {
+      key: '4',
+      name: 'Jim Red',
+      age: 18,
+      tel: '0575-22098909',
+      phone: 18900010002,
+      address: 'London No. 2 Lake Park',
+      rowSpan: 0,
+    },
+    {
+      key: '5',
+      name: 'Jake White',
+      age: 18,
+      tel: '0575-22098909',
+      phone: 18900010002,
+      address: 'Dublin No. 2 Lake Park',
+      rowSpan: null
     }
   ];
 }
